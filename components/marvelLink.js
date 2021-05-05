263b9b9fdcb184e970c8ebf5f54fef2b61c59f30
ahulterstrom <andrewhulterstrom@gmail.com>
2021-05-04 21:36:28 -0600

    Make buttons on [name] pages look good

--- components/marvelLink.js
+++ components/marvelLink.js
@@ -2,10 +2,14 @@ import Link from 'next/link'
 import utilStyles from '../styles/utils.module.scss'
 
 export default function marvelLink({ urlData }) {
+    let buttonText = urlData.type.slice();
+    if(buttonText == "comiclink"){
+        buttonText = "comics";
+    }
     return (
-        <div className="button">
+        <div className={utilStyles.button}>
             <Link href={urlData.url}>
-                <a>{urlData.type}</a>
+                <p>{buttonText.charAt(0).toUpperCase() + buttonText.slice(1)}</p>
             </Link>
         </div>
     )
