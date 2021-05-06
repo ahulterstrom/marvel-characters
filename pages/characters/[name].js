b607d5631fb8b2de19bb724e71ae0d646ebd6a2a
ahulterstrom <andrewhulterstrom@gmail.com>
2021-05-06 10:17:54 -0600

    Make search function send api requests from the client with debounce timer

--- pages/characters/[name].js
+++ pages/characters/[name].js
@@ -1,37 +1,35 @@
-import Layout from '../../components/mlayout'
+import Layout from '../../components/mlayout';
 // import Image from 'next/image'
-import Head from 'next/head'
-import utilStyles from '../../styles/utils.module.scss'
-import { getAllCharacterNames, getCharacterPageData } from '../../lib/characters'
-import MarvelLink from '../../components/marvelLink'
+import Head from 'next/head';
+import utilStyles from '../../styles/utils.module.scss';
+import { getAllCharacterNames, getCharacterPageData } from '../../lib/characters';
+import MarvelLink from '../../components/marvelLink';
 
 export async function getStaticProps({ params }) {
   let currentName = params.name;
 
-  if (currentName === "Araña") {
-    currentName = "Arana"
-  }
-  else if (currentName === "Edward \"Ted\" Forrester") {
-    currentName = "Edward%20%22Ted%22%20Forrester"
-  }
-  else if (currentName === "Dog Brother #1") {
-    currentName = "Dog%20Brother%20%231";
+  if (currentName === 'Araña') {
+    currentName = 'Arana';
+  } else if (currentName === 'Edward "Ted" Forrester') {
+    currentName = 'Edward%20%22Ted%22%20Forrester';
+  } else if (currentName === 'Dog Brother #1') {
+    currentName = 'Dog%20Brother%20%231';
   }
 
-  const characterData = await getCharacterPageData(currentName)
+  const characterData = await getCharacterPageData(currentName);
   return {
     props: {
-      characterData
-    }
-  }
+      characterData,
+    },
+  };
 }
 
 export async function getStaticPaths() {
-  const paths = await getAllCharacterNames()
+  const paths = await getAllCharacterNames();
   return {
     paths,
-    fallback: false
-  }
+    fallback: true,
+  };
 }
 
 export default function Character({ characterData }) {
@@ -51,10 +49,10 @@ export default function Character({ characterData }) {
         height={200}
         width={200}
         alt={characterData.pageData[0].name} /> */}
-        <img src={characterData.pageData[0].thumbnail.path + "." + characterData.pageData[0].thumbnail.extension} 
-        className={`${utilStyles.borderCircle} ${utilStyles.characterImage}`}
-        height={200}
-        width={200} />
+      <img src={characterData.pageData[0].thumbnail.path + '.' + characterData.pageData[0].thumbnail.extension}
+           className={`${utilStyles.borderCircle} ${utilStyles.characterImage}`}
+           height={200}
+           width={200} />
       <div>
         <p>{characterData.pageData[0].description}</p>
       </div>
@@ -65,5 +63,5 @@ export default function Character({ characterData }) {
       </div>
     </div>
 
-  </Layout>)
+  </Layout>);
 }
